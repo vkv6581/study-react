e4de8d68df091a34509d3845722ae83e250aa1ac
Lee.JaeYoung <vkv6581@gmail.com>
2021-08-10 22:03:21 +0900

    리액트 메인 화면 수정 테스트

--- src/App.js
+++ src/App.js
@@ -2,22 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let posts = '테스트 글 하나';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>상단 바</div>
+      </div>
+      <img src="{logo}"></img>
+      <h4 style={{color:'blue'}}>{posts}</h4>  데이터 바인딩.
     </div>
   );
 }
